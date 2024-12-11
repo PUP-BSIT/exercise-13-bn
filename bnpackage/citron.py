3a661404d66a57bb9cc3ae0d78226a8aeb474346
citron_kathleen <kccitron@gmail.com>
2024-12-11 17:52:43 +0800

    Add changes to citron branch (#3)
    * Add changes to citron branch
    
    * Add changes to citron branch.

---
+++ bnpackage/citron.py
@@ -0,0 +1,27 @@
+def kathleen_citron ():
+
+    choice = 0
+
+    while choice != 4:
+
+        print("Hello! My name is Kathleen C. Citron")
+        print("1. Basic Info")
+        print("2. Goals")
+        print("3. Comments")
+        print("4. Exit")
+        choice = int(input("Enter your choice: "))
+
+        match choice:
+
+            case 1:
+                print("I like vegetables.")
+                print("I'm 19 years old.")
+                print("I love playing online games.")
+
+            case 2:
+                print("My goal is to be a successful person.")
+                print("Improving my coding skills.")
+            case 3:
+                print("Opinions:")
+            case 4:
+                return            
